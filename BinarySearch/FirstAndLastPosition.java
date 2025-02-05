c766e23ac831d13ec188484dd914daa9a9d5a135
tnsqqq <tnsqqqbaliyan@gmail.com>
2025-02-06 01:02:16 +0530

    6

---
+++ BinarySearch/FirstAndLastPosition.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+
+public class FirstAndLastPosition {
+  public static void main(String[] args) {
+
+    int[] nums = { 5, 7, 7, 8, 8, 10 };
+    int[] output = searchRange(nums, 8);
+    System.out.println(Arrays.toString(output));
+
+  }
+
+  static int[] searchRange(int[] nums, int target) {
+
+    int[] ans = { -1, -1 };
+
+    // check for the first occurence of target first
+    int start = search(nums, target, true);
+    int end = search(nums, target, false);
+
+    ans[0] = start;
+    ans[1] = end;
+    return ans;
+
+  }
+
+  // this function just returns the index value of the target
+  static int search(int[] nums, int target, boolean findStartIndex) {
+    int ans = -1;
+    int start = 0;
+    int end = nums.length - 1;
+
+    while (start <= end) {
+
+      int mid = start + (end - start) / 2;
+
+      if (target < nums[mid]) {
+        end = mid - 1;
+      } else if (target > nums[mid]) {
+        start = mid + 1;
+      } else {
+        // potential ans found
+        ans = mid;
+        if (findStartIndex) {
+          end = mid - 1;
+        } else {
+          start = mid + 1;
+        }
+      }
+
+    }
+    return ans;
+  }
+}
