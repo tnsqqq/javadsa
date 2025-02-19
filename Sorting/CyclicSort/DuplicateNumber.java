e5bf7ce77fb345334873e1ef331ace99ec988e3a
tnsqqq <tnsqqqbaliyan@gmail.com>
2025-02-19 18:29:57 +0530

    21

---
+++ Sorting/CyclicSort/DuplicateNumber.java
@@ -0,0 +1,29 @@
+public class DuplicateNumber {
+  public static void main(String[] args) {
+    int[] arr = { 1, 3, 4, 2, 2 };
+    System.out.println(duplicateNumber(arr));
+  }
+
+  static int duplicateNumber(int[] arr) {
+    int i = 0;
+    while (i < arr.length) {
+      if (arr[i] != i + 1) {
+        int correct = arr[i] - 1;
+        if (arr[i] != arr[correct]) {
+          swap(arr, i, correct);
+        } else {
+          return arr[i];
+        }
+      }
+      i++;
+    }
+    return -1;
+  }
+
+  static void swap(int[] arr, int first, int second) {
+    int temp = arr[first];
+    arr[first] = arr[second];
+    arr[second] = temp;
+  }
+
+}
