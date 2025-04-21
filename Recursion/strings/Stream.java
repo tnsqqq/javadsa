45ed47861beafe8c3c35fd2ad34c4506ed951459
tnsqqq <tnsqqqbaliyan@gmail.com>
2025-04-21 15:30:55 +0530

    string

---
+++ Recursion/strings/Stream.java
@@ -0,0 +1,77 @@
+// public class Stream {
+//   public static void main(String[] args) {
+//     skip("", "baccas");
+//   }
+
+//   static void skip(String p, String up) {
+//     if (up.isEmpty()) {
+//       System.out.println(p);
+//       return;
+//     }
+
+//     char ch = up.charAt(0);
+
+//     if (ch == 'a') {
+//       skip(p, up.substring(1));
+//     } else {
+//       skip(p + ch, up.substring(1));
+//     }
+//   }
+// }
+
+// public class Stream {
+//   public static void main(String[] args) {
+//     System.out.println(skip("baacs"));
+//   }
+
+//   static String skip(String up) {
+//     if (up.isEmpty()) {
+//       return "";
+//     }
+
+//     char ch = up.charAt(0);
+
+//     if (ch == 'a') {
+//       return skip(up.substring(1));
+//     } else {
+//       return ch + skip(up.substring(1));
+//     }
+//   }
+// }
+
+// public class Stream {
+//   public static void main(String[] args) {
+//     System.out.println(skipApple("bcapple"));
+//   }
+
+//   static String skipApple(String up) {
+//     if (up.isEmpty()) {
+//       return "";
+//     }
+
+//     if (up.startsWith("apple")) {
+//       return skipApple(up.substring(5));
+//     } else {
+//       return up.charAt(0) + skipApple(up.substring(1));
+//     }
+//   }
+// }
+
+public class Stream {
+  public static void main(String[] args) {
+    System.out.println(skipAppNotApple("bcapp"));
+    System.out.println(skipAppNotApple("bcapple"));
+  }
+
+  static String skipAppNotApple(String up) {
+    if (up.isEmpty()) {
+      return "";
+    }
+
+    if (up.startsWith("app") && !up.startsWith("apple")) {
+      return skipAppNotApple(up.substring(3));
+    } else {
+      return up.charAt(0) + skipAppNotApple(up.substring(1));
+    }
+  }
+}
